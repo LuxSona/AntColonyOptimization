f0c64cf5d5d0a565258cd120bf810fa70f369814
Lux <ira.k.khan@outlook.com>
2026-08-20 05:41:55 -0600

    Added labels to TSP

--- TSP.py
+++ TSP.py
@@ -5,12 +5,15 @@ import numpy.typing as npt
 class TravellingSalesmanProblem():
     """
     Creates an instance of the Travelling Salesman Problem
+    :param labels: An n length set of labels for each city..
+    :type labels: lost[str],
 
     :param distances: An nxn (where n is the length of the number of cities) describing distances between col and row.
     :type distances: npt.NDArray[np.float64] 
     """
-    def __init__(self, distances: npt.NDArray[np.float64]):
+    def __init__(self, labels: list[str], distances: npt.NDArray[np.float64]):
         self.distances = distances
+        self.labels = labels
     
 
 
@@ -23,5 +26,5 @@ if __name__ == "__main__":
         [971,774,839,0,527],
         [878,1003,1019,527,0],
         ])
-    
-    problem = TravellingSalesmanProblem(distances)
+    labels = ["Denver", "Albuquerque", "Santa Fe", "Las Vegas", "Elko"]
+    problem = TravellingSalesmanProblem(labels, distances)
